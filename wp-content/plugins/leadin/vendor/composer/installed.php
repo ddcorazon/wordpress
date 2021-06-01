450286979d61675ce2eea10484ed31adfe330b61
cdabcompass <tech@cdabcompass.com>
2021-06-01 19:28:03 +0200

    commit 1

---
+++ wp-content/plugins/leadin/vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '4b05a4877f68989c50214543e6aebeeaa5fd7a23',
+    'name' => 'hubspot/leadin-wordpress-plugin',
+  ),
+  'versions' => 
+  array (
+    'hubspot/leadin-wordpress-plugin' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '4b05a4877f68989c50214543e6aebeeaa5fd7a23',
+    ),
+  ),
+);
